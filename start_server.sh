cc49b9cee79789f0a92a19d22d96dac6901d3776
Jim Ng <tyt03538@gmail.com>
2017-02-07 01:58:03 +0000

    improve the starut up script to return the server status

--- start_server.sh
+++ start_server.sh
@@ -2,3 +2,6 @@
 
 node /home/tyt03538/FanTravelBackEnd/server.js > /home/tyt03538/logs/stdout.log 2> /home/tyt03538/logs/stderr.log &
 
+status="$(curl -Is http://23.251.157.86:8080/api/_health | head -n 1)"
+echo "$status"
+
